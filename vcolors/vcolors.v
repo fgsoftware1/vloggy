bc8b9d0aa0fccc4674d9ab5c8d1e97bc6cb0c9e7
babywolf <fgsoftwarestudio@gmail.com>
2025-07-01 17:02:27 +0100

    - random color

--- vcolors/vcolors.v
+++ vcolors/vcolors.v
@@ -1,5 +1,7 @@
 module vcolors
 
+import rand
+
 pub const reset = '\x1b[0m'
 pub const bold = '\x1b[1m'
 pub const red = '\x1b[31m'
@@ -8,3 +10,10 @@ pub const yellow = '\x1b[33m'
 pub const blue = '\x1b[34m'
 pub const cyan = '\x1b[36m'
 pub const white = '\x1b[37m'
+
+const colors = [red, green, yellow, blue, cyan, white]
+
+pub fn print_random_color(text string) {
+	random_color := colors[rand.intn(colors.len) or { 0 }]
+	print('${random_color}${text}${reset}')
+}
